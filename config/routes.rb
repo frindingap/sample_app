c26e9e49c87bb77d2e9f5032083531798e665b76
Alberto Blanco <albertoblanco.s@gmail.com>
2014-05-22 11:54:32 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  resources :users
   match '/signup', to: 'users#new', via: 'get'
   root 'static_pages#home'
   match '/about',   to: 'static_pages#about',via: 'get'
