30d619cb112ba15a4719c139601a4930fec11c71
Alberto Blanco <albertoblanco.s@gmail.com>
2014-05-28 13:20:32 -0700

    Remove 140 char limitation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ActiveRecord::Base
 	belongs_to :user
 	default_scope -> {order('created_at DESC')}
 	validates :user_id, presence: true
-	validates :content, presence: true, length: {maximum:140}
+	validates :content, presence: true #, length: {maximum:140}
 
  # Returns microposts from the users being followed by the given user.
   def self.from_users_followed_by(user)
