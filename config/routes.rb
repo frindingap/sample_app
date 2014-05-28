c9238a1749341d53f3490b6576c2dadda7244f6d
Alberto Blanco <albertoblanco.s@gmail.com>
2014-05-27 23:34:54 -0700

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :sessions, only:[:new,:create,:destroy]
-  resources :users
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   match '/signup', to: 'users#new', via: 'get'
   root 'static_pages#home'
   match '/about',   to: 'static_pages#about',via: 'get'
