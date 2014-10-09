1445f063bd03fdb88a0d78fd378ac2640012be08
Alberto Blanco <albertoblanco.s@gmail.com>
2014-10-09 16:48:47 -0700

    change landing page

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ SampleApp::Application.routes.draw do
   end
   resources :microposts,    only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
+  root  'feeds#index'
   match '/signup', to: 'users#new', via: 'get'
-  root 'static_pages#home'
+  #root 'static_pages#home'
   match '/about',   to: 'static_pages#about'  ,via: 'get'
   match '/help',    to: 'static_pages#help'   ,via: 'get'
   match '/contact', to: 'static_pages#contact',via: 'get'
